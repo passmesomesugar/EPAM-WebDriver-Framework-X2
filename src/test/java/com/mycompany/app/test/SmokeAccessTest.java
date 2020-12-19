45a8678110d79222ced2a4b15bfa07f13104271f
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2020-12-19 13:20:43 +0600

    smoke test off

--- src/test/java/com/mycompany/app/test/SmokeAccessTest.java
+++ src/test/java/com/mycompany/app/test/SmokeAccessTest.java
@@ -1,25 +1,25 @@
-package com.mycompany.app.test;
-
-import com.mycompany.app.driver.DriverSingleton;
-import com.mycompany.app.model.CalculatorModel;
-import com.mycompany.app.page.CalculatorPage;
-import com.mycompany.app.page.HomePage;
-import com.mycompany.app.service.CalculatorCreator;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.Test;
-
-import java.awt.datatransfer.UnsupportedFlavorException;
-import java.io.IOException;
-
-public class SmokeAccessTest extends CommonConditions {
-    private WebDriver driver = DriverSingleton.getDriver();
-    private CalculatorModel calculatorModel = CalculatorCreator.withCredentialsFromProperty();
-
-    @Test
-    public void openPage() throws IOException, UnsupportedFlavorException {
-        CalculatorPage calculatorPage = new HomePage(driver)
-                .openPage()
-                .returnSearchResult("Google Cloud Platform Pricing Calculator")
-                .openCalculator();
-    }
-}
+//package com.mycompany.app.test;
+//
+//import com.mycompany.app.driver.DriverSingleton;
+//import com.mycompany.app.model.CalculatorModel;
+//import com.mycompany.app.page.CalculatorPage;
+//import com.mycompany.app.page.HomePage;
+//import com.mycompany.app.service.CalculatorCreator;
+//import org.openqa.selenium.WebDriver;
+//import org.testng.annotations.Test;
+//
+//import java.awt.datatransfer.UnsupportedFlavorException;
+//import java.io.IOException;
+//
+//public class SmokeAccessTest extends CommonConditions {
+//    private WebDriver driver = DriverSingleton.getDriver();
+//    private CalculatorModel calculatorModel = CalculatorCreator.withCredentialsFromProperty();
+//
+//    @Test
+//    public void openPage() throws IOException, UnsupportedFlavorException {
+//        CalculatorPage calculatorPage = new HomePage(driver)
+//                .openPage()
+//                .returnSearchResult("Google Cloud Platform Pricing Calculator")
+//                .openCalculator();
+//    }
+//}
